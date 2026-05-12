a435988e6e0642a673b0f7e9ceff449284a370ee
kpbolinas <bolinas.kurt@gmail.com>
2026-05-12 10:02:49 +0800

    Adjustments for date picker

--- src/app/components/DatePicker.tsx
+++ src/app/components/DatePicker.tsx
@@ -11,18 +11,20 @@ export default function DatePickerComponent({
   setDate: React.Dispatch<React.SetStateAction<Date | null>>;
 }) {
   return (
-    <DatePicker
-      selected={selected}
-      onChange={(date: Date | null) => setDate(date)}
-      placeholderText={placeholderValue || "Select Date"}
-      dateFormat="yyyy-MM-dd"
-      className="
+    <div className="relative">
+      <DatePicker
+        selected={selected}
+        onChange={(date: Date | null) => setDate(date)}
+        placeholderText={placeholderValue || "Select Date"}
+        dateFormat="yyyy-MM-dd"
+        className="
         w-full rounded-lg border border-gray-700
         bg-gray-900 text-white text-sm px-2 py-1
       "
-      calendarClassName="!bg-gray-900 !text-white border border-gray-700 rounded-lg"
-      dayClassName={() => "hover:bg-gray-700 rounded-md text-white"}
-      popperClassName="z-50"
-    />
+        calendarClassName="!bg-gray-900 !text-white border border-gray-700 rounded-lg"
+        dayClassName={() => "hover:bg-gray-700 rounded-md text-white"}
+        popperClassName="z-50"
+      />
+    </div>
   );
 }
